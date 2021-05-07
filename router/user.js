df307c10151992455e42108d67a80ba09bcba202
amir hossein norouzzadeh <78879226+amir-norouzzadeh@users.noreply.github.com>
2021-05-08 00:07:16 +0430

    Manage registration form errors
    ▲ Manage registration errors with Yup package

--- router/user.js
+++ router/user.js
@@ -1,6 +1,27 @@
 const { Router } = require("express");
+const Yup = require("yup");
 
 const router = new Router();
+
+
+
+const schema = Yup.object().shape({
+    fullname: Yup.string()
+        .required("نام و نام خانوادگی الزامی می باشد")
+        .min(4, "نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد")
+        .max(255, "نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد"),
+    email: Yup.string()
+        .email("ایمیل معتبر نمی باشد")
+        .required("ایمیل الزامی می باشد"),
+    password: Yup.string()
+        .min(4, "کلمه عبور نباید کمتر از 4 کاراکتر باشد")
+        .max(255, "کلمه عبور نباید بیشتر از 255 کاراکتر باشد")
+        .required("کلمه عبور الزامی می باشد"),
+    confirmPassword: Yup.string()
+        .required("تکرار کلمه عبور الزامی می باشد")
+        .oneOf([Yup.ref("password"), null],"کلمه عبور یکسان نیست"),
+});
+
 
 router.get("/register", (req, res) => {
     res.render("register", {
@@ -17,7 +38,19 @@ router.get("/login", (req, res) => {
 })
 
 router.post("/register", (req, res) => {
-    console.log(req.body);
+    schema.validate(req.body)
+.then(RESULT => {
+    console.log(RESULT);
+    res.redirect("/user/login");
+}).catch((ERR) => { 
+    console.log(ERR.errors)
+    res.render("register", {
+        pageTitle: "ثبت نام | خطا",
+        path: "/css/signin.css",
+        errors: ERR.errors,
+    });
+})
+
 });
 
 module.exports = router;
